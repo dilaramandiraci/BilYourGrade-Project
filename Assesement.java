a3fbd7efa1565a4af508440c03f0e5a53b23954f
dilaramandiraci <91568158+dilaramandiraci@users.noreply.github.com>
2022-04-06 14:16:51 +0300

    Add files via upload

---
+++ Assesement.java
@@ -0,0 +1,28 @@
+public class Assesement {
+    private String name;
+    private int weight;
+
+    public Assesement(String aName, int aweight)
+    {
+        this.name = aName;
+        this.weight = aweight;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    
+}
