b2c10f69f685428d1f1fd7bce077248f239800c8
Dilara Mandıracı <dilaramandiracii@gmail.com>
2022-04-20 14:56:39 +0300

    course class oluşturuldu
    assesment and name inner variables

--- Course.java
+++ Course.java
@@ -2,8 +2,29 @@ import java.util.ArrayList;
 
 public class Course {
     private ArrayList <Assesement> assesements;
+    private String name;
 
-    public Course()
-    {};
+    public Course(String aName, ArrayList<Assesement> assesments)
+    {
+        this.name = aName;
+        this.assesements = assesments;
+    }
+
+    public ArrayList<Assesement> getAssesements() {
+        return assesements;
+    }
+
+    public void setAssesements(ArrayList<Assesement> assesements) {
+        this.assesements = assesements;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     
 }
