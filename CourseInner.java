c4a8422bd63084255049f3b3c3a8aa0bbdb1846d
Barkın Çınar <barkin.cinar@ug.bilkent.edu.tr>
2022-05-08 23:08:22 +0300

    working GUI of course inner

--- CourseInner.java
+++ CourseInner.java
@@ -26,7 +26,6 @@ import java.util.Random;
 import javax.swing.Timer;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
@@ -45,7 +44,11 @@ public class CourseInner extends JFrame{
     private static final int FRAME_WIDTH = 1000;
     private static final int FRAME_HEIGHT = 700;
 
+    Random rand = new Random();
+
+
     JButton backButton = new JButton();
+    JLabel courseName = new JLabel();
 
     //CONSTRUCTOR
     public CourseInner(){
@@ -56,18 +59,50 @@ public class CourseInner extends JFrame{
 
 
         add(getBackButton());
+        add(getCourseName());
+        for(int i = 0; i < 5 ; i++)//TO DO -> 5 yerine course un assesement sayısı gelicek
+        {
+            add(getAssesement(i));
+        }
+        
+
         JLabel label = new JLabel("only pain :/");
+
         add(label);
     }
     
     public JButton getBackButton(){
         backButton.setText("<--");
         backButton.setSize(60, 60);
-        backButton.setLocation(30,30);
+        backButton.setLocation(20,20);
         backButton.setForeground(Color.WHITE);
         backButton.setBackground(Color.BLACK);
         return backButton;
     }
+    
+    public JLabel getCourseName(){
+        courseName.setText("TO DO (MATH102)");
+        courseName.setSize(300, 60);
+        courseName.setLocation(100,20);
+        return courseName;
+    }
+
+    public JPanel getAssesement(int i) {
+        JPanel assesement = new JPanel();
+        JLabel assesementName = new JLabel();
+        assesementName.setText("assesement name " + i); // TO DO -> thiscourse.assesements[i].getName();
+        assesementName.setSize(300, 60);
+        assesementName.setLocation(20,100);
+        assesement.setSize(500, 100);
+        assesement.setLocation(20,120 + (i*100));
+        assesement.setBackground(new java.awt.Color(rand.nextInt(200),rand.nextInt(200),rand.nextInt(200)));
+        assesement.add(assesementName);
+
+
+
+        return assesement;
+    }
+
 
     public static void main(String[] args) {
         
